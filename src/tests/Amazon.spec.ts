28fd3c29f17179652a327e6caa997c49cfd3ed0f
DhanrajKamble5205 <dhanrajkamble5205@gmail.com>
2025-10-08 15:28:00 +0530

    final

--- src/tests/Amazon.spec.ts
+++ src/tests/Amazon.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test ('The first test', async({page}) => {
+test ('Amazon Mobile selection and click on Buy option', async({page}) => {
 await page.goto("https://www.amazon.in/")
 
 await page.getByText('Mobile').click()
 await page.getByText('Apple iPhone 15 (128 GB) - Black').first().click()
 await page.locator('#add-to-cart-button').nth(1).click();
-await page.getByText('Added to cart').isVisible
+await page.getByText('Added to cart').isVisible()
 await page.locator('#sc-buy-box-ptc-button').click()
-})
 
-test ('Finding Child element', async({page}) => {
-    await page.locator('')
+await page.locator('#ap_email_login').fill('kdhanraj21@gmail.com')
+await page.locator('#continue').click()
+await page.getByRole('button', { name: 'Send OTP' }).click();
+
 })
