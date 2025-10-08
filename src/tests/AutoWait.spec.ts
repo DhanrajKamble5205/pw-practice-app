73c20a6f30436cbe56f68478073aa1f8f62a99c5
DhanrajKamble5205 <dhanrajkamble5205@gmail.com>
2025-10-08 16:13:41 +0530

    autowait

---
+++ src/tests/AutoWait.spec.ts
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test';
+
+test.beforeEach(async({page}) => {
+
+    await page.goto('http://www.uitestingplayground.com/ajax')
+    await page.locator('#ajaxButton').click()
+
+})
+
+test('Auto wait checking',async({page}) => {
+    const sucessbut =  page.locator('.bg-success')
+    await sucessbut.click()
+})
